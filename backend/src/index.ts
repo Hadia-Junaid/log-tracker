cb551888cf3d4d51f54532820c09d803eb6ec637
raahem <25100091@lums.edu.pk>
2025-06-25 11:32:38 +0500

    Added morgan for logging HTTP Requests automatically

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,10 +2,11 @@ import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 import mongoose from "mongoose";
-import logger from "./utils/logger";
+import logger, {morganStream} from "./utils/logger";
 import errorHandler from "./middleware/error";
 import { processErrors } from "./startup/processErrors";
 import config from "config";
+import morgan from "morgan";
 
 processErrors(); // Initialize process level error handlers
 
@@ -15,6 +16,8 @@ const mongoUri = config.get<string>("mongoUri") || "";
 const app = express();
 
 app.use(express.json());
+app.use(morgan('tiny', { stream: morganStream }));
+
 
 mongoose.connect(mongoUri)
   .then(() => {
