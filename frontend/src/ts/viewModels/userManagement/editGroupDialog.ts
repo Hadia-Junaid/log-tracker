b6c5ef77f4b98a59f486e9a567f494cee0d5dcd3
Raahem Nabeel <108220660+raahemn@users.noreply.github.com>
2025-07-03 16:18:06 +0500

    post edit showing dropdowns

--- frontend/src/ts/viewModels/userManagement/editGroupDialog.ts
+++ frontend/src/ts/viewModels/userManagement/editGroupDialog.ts
@@ -231,6 +231,7 @@ export const editGroupDialogMethods = {
       const groupIndex = updatedGroups.findIndex((g) => g.groupId === groupId);
 
       if (groupIndex !== -1) {
+        console.log("Updating group in observable array:", updatedGroup);
         const createdDate = new Date(updatedGroup.createdAt);
         updatedGroups[groupIndex] = {
           groupId: updatedGroup._id,
@@ -242,10 +243,15 @@ export const editGroupDialogMethods = {
           createdDate: createdDate.toLocaleDateString(),
           createdAgo: groupListMethods.getRelativeTime(createdDate),
           is_admin: updatedGroup.is_admin,
+          members: updatedGroup.members.map((member: any) => member.email),
+          assigned_applications: updatedGroup.assigned_applications.map(
+            (app: any) => app.name
+          ),
         };
 
         // Force the observableArray to update
         groupListObservables.groupDataArray.valueHasMutated();
+        
       }
 
       // Show success message using globalBanner
