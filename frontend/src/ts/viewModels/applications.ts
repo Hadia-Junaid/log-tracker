dd1ec277b4456da399b7ec689f1e4c28b0ca5ca1
Hadia-Junaid <hadia.junaid@gosaas.io>
2025-07-03 12:02:10 +0000

    app validation fix

--- frontend/src/ts/viewModels/applications.ts
+++ frontend/src/ts/viewModels/applications.ts
@@ -69,6 +69,7 @@ class ApplicationViewModel {
     statusFilterOptions = statusFilterOptions;
     environmentFilterOptions = environmentFilterOptions;
     private authService: AuthService;
+    addAppDialogError = addAppDialogObservables.addAppDialogError;
 
     // Computed
     readonly totalPages = applicationListComputed.totalPages;
