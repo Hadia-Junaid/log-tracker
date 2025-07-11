6c8a3221c4970a754a4f9091865595adf8b19df9
raahemn <25100091@lums.edu.pk>
2025-07-11 14:45:02 +0500

    Header and Sidebar adjusted with user context

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,18 +1,21 @@
 import { registerCustomElement } from "ojs/ojvcomponent";
 import { h } from "preact";
 import { UserProvider } from "./context/UserContext";
-import {AuthenticatedApp} from "./AuthenticatedApp";
+import { AuthenticatedApp } from "./AuthenticatedApp";
 
 type Props = {
-  appName?: string;
-  userLogin?: string;
+    appName?: string;
+    userLogin?: string;
 };
 
 // This component simply serves as an entry point to the app and provides the UserContext to the entire application.
-export const App = registerCustomElement("app-root", ({ appName = "" }: Props) => {
-  return (
-    <UserProvider>
-      <AuthenticatedApp appName={appName} />
-    </UserProvider>
-  );
-});
+export const App = registerCustomElement(
+    "app-root",
+    ({ appName = "" }: Props) => {
+        return (
+            <UserProvider>
+                <AuthenticatedApp appName={appName} />
+            </UserProvider>
+        );
+    }
+);
